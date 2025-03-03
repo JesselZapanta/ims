3932e455577c168e57d0964755e99b2b30ef6e94
JesselZapanta <jesselzapanta9@gmail.com>
2025-03-03 20:51:29 +0800

    user table

--- resources/js/Pages/Admin/User/Index.jsx
+++ resources/js/Pages/Admin/User/Index.jsx
@@ -2,6 +2,17 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { useEffect, useState } from "react";
 
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+
 function Index() {
 
     const [data, setData] = useState([]);
@@ -35,14 +46,34 @@ function Index() {
             }
         >
             <Head title="User Management" />
-            <div className="p-6 text-gray-900">List of users</div>
-            <div>
+            <div className="p-6 text-gray-900">
+                <div className="mb-4">List of users</div>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>ID</TableHead>
+                            <TableHead>Name</TableHead>
+                            <TableHead>Email</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {data.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            {/* <div>
                 {
                     data.map((user) => (
                         <li key={user.id}>{user.name}</li>
                     ))
                 }
-            </div>
+            </div> */}
         </AuthenticatedLayout>
     );
 }
